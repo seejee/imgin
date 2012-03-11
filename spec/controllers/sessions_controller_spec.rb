505bee2ab28e5902b69214cd320f2feafd1a4ef0
Chris Geihsler <chris@geihsler.net>
2012-03-11 13:39:40 -0400

    fixing spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,24 +1,24 @@
-require 'spec_helper'
+require "spec_helper"
 
-class User
-  def id
+class FakeUser
+  def id 
     42
   end
 end
 
 describe SessionsController do
 
-  context 'when receiving an omniauth callback' do
+  context "when receiving an omniauth callback" do
 
     let(:omni_auth) { Hash.new }
-    let(:user)      { User.new }
+    let(:user)      { FakeUser.new }
 
     before(:each) do
-      subject.should_receive(:omni_auth).and_return(omni_auth)
-      User.should_receive(:find_or_create_from_omni_auth).with(omni_auth).and_return(user)
+      subject.stub(:omni_auth) { omni_auth }
+      User.stub(:find_or_create_from_omni_auth).with(omni_auth) { user }
     end
 
-    it 'should save the user id in the session' do
+    it "should save the user id in the session" do
       post :create
       session[:user_id].should == 42
     end
