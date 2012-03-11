4be8f0911e4e8c8910070e02bca5a0e4aebd9900
Chris Geihsler <chris@geihsler.net>
2012-03-11 16:48:58 -0400

    fixing spec language

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,5 @@
 require "spec_helper"
 
-
-
-describe User do
-
-  context "when finding or creating via omni auth" do
-  
-    let(:omni_auth) { build_omni_auth }
-
-    context "when a user does not exist exist with that github uid" do
-      subject         { User.find_or_create_from_omni_auth(omni_auth) }
-
-      its(:id)         { should_not == nil }
-      its(:name)       { should == "Test Guy" }
-      its(:email)      { should == "test@test.com" }
-      its(:github)     { should == "github" }
-      its(:github_uid) { should == "17" }
-    end
-
-    context "when a user does not exist exist with that github uid" do
-
-      before(:each) do
-        User.find_or_create_from_omni_auth(omni_auth)
-      end
-        
-      subject   { User.find_or_create_from_omni_auth(omni_auth) }
-
-      its(:github_uid) { should == "17" }
-    end
-  end
-end
-
 def build_omni_auth
   {
     :provider => "github",
@@ -41,5 +10,34 @@ def build_omni_auth
           :nickname => "github"
       }
   }
+end
+
+describe User do
+
+  context "when finding or creating via omni auth" do
+  
+    let(:omni_auth) { build_omni_auth }
+
+    context "when a user does not exist with that github uid" do
+      subject         { User.find_or_create_from_omni_auth(omni_auth) }
+
+      its(:id)         { should_not == nil }
+      its(:name)       { should == "Test Guy" }
+      its(:email)      { should == "test@test.com" }
+      its(:github)     { should == "github" }
+      its(:github_uid) { should == "17" }
+    end
+
+    context "when a user already exists with that github uid" do
+
+      before(:each) do
+        User.find_or_create_from_omni_auth(omni_auth)
+      end
+        
+      subject   { User.find_or_create_from_omni_auth(omni_auth) }
+
+      its(:github_uid) { should == "17" }
+    end
+  end
 end
 
