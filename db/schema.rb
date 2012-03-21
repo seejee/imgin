6d1b16fb5429d4e357eb0b778fec07504d740a35
Chris Geihsler <chris@geihsler.net>
2012-03-20 23:45:28 -0400

    renamed image.filename to image.file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320012117) do
+ActiveRecord::Schema.define(:version => 20120321034313) do
 
   create_table "images", :force => true do |t|
     t.string   "description"
-    t.string   "filename"
+    t.string   "file"
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
